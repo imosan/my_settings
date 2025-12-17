c903a7b3352ea245ebe6bf9b20feda95d478f6d6
Hirotaka Imagawa <imosan@mac.com>
2025-12-17 17:28:50 +0900

    Update ThumbnailExporter.cpp

--- ThumbnailExporter.cpp
+++ ThumbnailExporter.cpp
@@ -1,45 +1,58 @@
+// ThumbnailExporter.cpp
 #include "ThumbnailExporter.h"
 
 #include "AssetRegistry/AssetRegistryModule.h"
+#include "AssetRegistry/IAssetRegistry.h"
+#include "UObject/SoftObjectPath.h"
+
+#include "UnrealEd/Public/ThumbnailTools.h"
 #include "UnrealEd/Public/AssetThumbnail.h"
 #include "UnrealEd/Public/AssetThumbnailPool.h"
+
 #include "ImageUtils.h"
 #include "Misc/FileHelper.h"
 
-void FThumbnailExporterModule::StartupModule() {}
-void FThumbnailExporterModule::ShutdownModule() {}
+IMPLEMENT_MODULE(FThumbnailExporterModule, ThumbnailExporter)
 
-bool FThumbnailExporterModule::ExportThumbnail(
-    const FString& AssetPath,
-    const FString& OutPNGPath)
+bool FThumbnailExporterModule::ExportThumbnailPNG(
+    const FString& ObjectPath,
+    const FString& OutputPNG)
 {
+    // AssetRegistry
     FAssetRegistryModule& ARM =
         FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
 
-    FAssetData AssetData = ARM.Get().GetAssetByObjectPath(*AssetPath);
-    if (!AssetData.IsValid())
+    FAssetData AssetData;
+    if (ARM.Get().TryGetAssetByObjectPath(
+            FSoftObjectPath(ObjectPath),
+            AssetData
+        ) != UE::AssetRegistry::EExists::Exists)
+    {
         return false;
+    }
 
-    TSharedPtr<FAssetThumbnailPool> Pool =
+    // Thumbnail pool
+    TSharedRef<FAssetThumbnailPool> Pool =
         MakeShared<FAssetThumbnailPool>(1);
 
+    // Create thumbnail
     FAssetThumbnail Thumbnail(AssetData, 256, 256, Pool);
     Thumbnail.RefreshThumbnail();
 
+    // Read pixels
     FSlateShaderResource* Resource =
         Thumbnail.GetViewportRenderTargetTexture();
     if (!Resource)
         return false;
 
     TArray<FColor> Pixels;
-    Pixels.SetNum(256 * 256);
+    Pixels.SetNumUninitialized(256 * 256);
 
     Resource->ReadPixels(Pixels);
 
+    // Encode PNG
     TArray<uint8> PNG;
     FImageUtils::CompressImageArray(256, 256, Pixels, PNG);
 
-    return FFileHelper::SaveArrayToFile(PNG, *OutPNGPath);
+    return FFileHelper::SaveArrayToFile(PNG, *OutputPNG);
 }
-
-IMPLEMENT_MODULE(FThumbnailExporterModule, ThumbnailExporter)
