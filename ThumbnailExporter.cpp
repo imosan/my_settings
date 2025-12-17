ebb4102a7440d7e2979939cc32340c02beb1846a
Hirotaka Imagawa <imosan@mac.com>
2025-12-17 17:31:30 +0900

    Update ThumbnailExporter.cpp

--- ThumbnailExporter.cpp
+++ ThumbnailExporter.cpp
@@ -1,14 +1,18 @@
-// ThumbnailExporter.cpp
+
 #include "ThumbnailExporter.h"
 
 #include "AssetRegistry/AssetRegistryModule.h"
 #include "AssetRegistry/IAssetRegistry.h"
 #include "UObject/SoftObjectPath.h"
 
-#include "UnrealEd/Public/ThumbnailTools.h"
 #include "UnrealEd/Public/AssetThumbnail.h"
 #include "UnrealEd/Public/AssetThumbnailPool.h"
 
+#include "Rendering/SlateRenderer.h"
+#include "Slate/SlateTextures.h"
+
+#include "RHI.h"
+#include "RenderResource.h"
 #include "ImageUtils.h"
 #include "Misc/FileHelper.h"
 
@@ -24,35 +28,47 @@ bool FThumbnailExporterModule::ExportThumbnailPNG(
 
     FAssetData AssetData;
     if (ARM.Get().TryGetAssetByObjectPath(
-            FSoftObjectPath(ObjectPath),
-            AssetData
-        ) != UE::AssetRegistry::EExists::Exists)
+        FSoftObjectPath(ObjectPath),
+        AssetData) != UE::AssetRegistry::EExists::Exists)
     {
         return false;
     }
 
-    // Thumbnail pool
+    // Thumbnail
     TSharedRef<FAssetThumbnailPool> Pool =
         MakeShared<FAssetThumbnailPool>(1);
 
-    // Create thumbnail
     FAssetThumbnail Thumbnail(AssetData, 256, 256, Pool);
     Thumbnail.RefreshThumbnail();
 
-    // Read pixels
-    FSlateShaderResource* Resource =
-        Thumbnail.GetViewportRenderTargetTexture();
-    if (!Resource)
+    // ★ 正しい型で取得
+    FSlateTexture2DRHIRef* SlateTex =
+        static_cast<FSlateTexture2DRHIRef*>(
+            Thumbnail.GetViewportRenderTargetTexture());
+
+    if (!SlateTex || !SlateTex->GetTypedResource())
         return false;
 
+    FTexture2DRHIRef Tex = SlateTex->GetTypedResource();
+
+    // Read pixels via RHI
     TArray<FColor> Pixels;
-    Pixels.SetNumUninitialized(256 * 256);
+    FIntRect Rect(0, 0, 256, 256);
 
-    Resource->ReadPixels(Pixels);
+    FRHICommandListImmediate& RHICmd =
+        FRHICommandListExecutor::GetImmediateCommandList();
 
-    // Encode PNG
+    RHICmd.ReadSurfaceData(
+        Tex,
+        Rect,
+        Pixels,
+        FReadSurfaceDataFlags(RCM_UNorm)
+    );
+
+    // PNG encode
     TArray<uint8> PNG;
     FImageUtils::CompressImageArray(256, 256, Pixels, PNG);
 
+    // ★ 第2引数は TCHAR*
     return FFileHelper::SaveArrayToFile(PNG, *OutputPNG);
 }
